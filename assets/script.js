ddef1e9be08e81007db33335f2ea676ba757ed2b
bfrazier <bthehippie2@gmail.com>
2022-11-27 15:43:41 -0500

    got input on 8am block to save to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,16 @@
 let date = document.getElementById('date');
+let task = document.getElementById('task-input');
+let save8 = document.getElementById('8-save');
 
 date.textContent = dayjs().format('MMM D, YYYY');
+
+
+save8.onclick = function() {
+    const info = task.value;
+    localStorage.setItem('info', info) 
+};
+
+
+
+
+

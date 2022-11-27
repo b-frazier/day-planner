bce79df42acc1e8fe58ed9d003c490db73ecad68
bfrazier <bthehippie2@gmail.com>
2022-11-27 16:08:51 -0500

    display time of day

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
 let date = document.getElementById('date');
+let time = document.getElementById('time');
 let task = document.getElementById('task-input');
 let save8 = document.getElementById('8-save');
 
 date.textContent = dayjs().format('MMM D, YYYY');
-
+time.textContent = dayjs().format('h:mm a');
 
 save8.onclick = function() {
     const info = task.value;
